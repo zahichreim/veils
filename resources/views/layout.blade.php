b1d4dc668e490b94ff731f9e9a5e16364d0ae147
zahi-chreim <zahi.chreim@basolutions-lb.com>
2025-04-28 12:36:52 +0300

    fixing copyright

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -231,7 +231,7 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 2.0
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; {{ \Carbon\Carbon::now()->year }} <a href="#">PaceX</a>.</strong> All rights reserved.
       </footer>
     </div><!-- ./wrapper -->
 
--- resources/views/pacex/layout.blade.php
+++ resources/views/pacex/layout.blade.php
@@ -390,7 +390,7 @@
 
 				<p class="stext-107 cl6 txt-center">
 					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+Copyright &copy;{{ \Carbon\Carbon::now()->year }} All rights reserved | Pacex | Privacy and Terms.
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 
 				</p>
